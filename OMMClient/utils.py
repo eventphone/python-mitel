9214e6153fbe2630ede464bc18192aa90e654979
thomasDOTde <thomas@drewermann.org>
2017-11-08 19:57:32 +0100

    fixed typos

--- OMMClient/utils.py
+++ OMMClient/utils.py
@@ -4,7 +4,7 @@ import base64
 
 def encrypt_pin(pin, modulus, exponent):
     # Known weak implementation of encryption using ECB
-    # Neccessary for OMMM to compare PIN of a user
+    # Necessary for OMM to compare PIN of a user
     pub_key = rsa.PublicKey(int(modulus, 16), int(exponent, 16))
     crypted = rsa.encrypt(pin, pub_key=pub_key)
     return base64.b64encode(crypted)
