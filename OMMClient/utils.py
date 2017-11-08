2010cbc1f75c6581f71f0d7d592cfee1dc0325b2
thomasDOTde <thomas@drewermann.org>
2017-11-08 20:39:42 +0100

    added reference

--- OMMClient/utils.py
+++ OMMClient/utils.py
@@ -4,6 +4,8 @@ import base64
 
 def encrypt_pin(pin, modulus, exponent):
     # Known weak implementation of encryption using ECB
+    # "Chaining mode such as ECB makes no sense for RSA, unless you are doing it wrong."
+    # Quote from https://stackoverflow.com/questions/2855326
     # Necessary for OMM to compare PIN of a user
     pub_key = rsa.PublicKey(int(modulus, 16), int(exponent, 16))
     crypted = rsa.encrypt(pin, pub_key=pub_key)
